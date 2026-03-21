396798bcccaade181e8600f2923022f0a478404d
specificuser1 <motorolag14rx@gmail.com>
2026-03-21 12:01:48 +0500

    Update get-settings.php

--- api/get-settings.php
+++ api/get-settings.php
@@ -1,10 +1,9 @@
 <?php
 require "../config.php";
 
-if (!isset($_SERVER["HTTP_API_KEY"]) || $_SERVER["HTTP_API_KEY"] !== $secret_key) {
+if ($_SERVER["HTTP_API_KEY"] !== $secret_key) {
     http_response_code(403);
     die("FORBIDDEN");
 }
 
-header("Content-Type: application/json");
-echo file_get_contents("../settings.json");
+echo file_get_contents($settings_file);
