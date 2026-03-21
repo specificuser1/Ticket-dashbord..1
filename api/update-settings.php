e8e816874088093134675e7c376767e40b7a09a9
specificuser1 <motorolag14rx@gmail.com>
2026-03-21 12:02:47 +0500

    Update update-settings.php

--- api/update-settings.php
+++ api/update-settings.php
@@ -1,13 +1,12 @@
 <?php
 require "../config.php";
 
-if (!isset($_SERVER["HTTP_API_KEY"]) || $_SERVER["HTTP_API_KEY"] !== $secret_key) {
+if ($_SERVER["HTTP_API_KEY"] !== $secret_key) {
     http_response_code(403);
     die("FORBIDDEN");
 }
 
 $data = json_decode(file_get_contents("php://input"), true);
-
-file_put_contents("../settings.json", json_encode($data, JSON_PRETTY_PRINT));
+file_put_contents($settings_file, json_encode($data, JSON_PRETTY_PRINT));
 
 echo json_encode(["status" => "UPDATED"]);
